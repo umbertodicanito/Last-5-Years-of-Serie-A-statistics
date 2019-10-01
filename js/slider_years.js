26ce96063f73922b03222d6e189aeeee14bbdf1a
Umberto di Canito <umbertodicanito@gmail.com>
2019-10-01 19:13:35 +0200

    Add files via upload

---
+++ js/slider_years.js
@@ -0,0 +1,12 @@
+var values_seasons = ["2014/15", "2015/16", "2016/17", "2017/18", "2018/19"];
+var season_from = values_seasons.indexOf("2015/16");
+var season_to = values_seasons.indexOf("2017/18");
+
+$(".js-range-slider").ionRangeSlider({
+    type: "double",
+    grid: true,
+    from: season_from,
+    to: season_to,
+    step: 1,
+    values: values_seasons
+});
